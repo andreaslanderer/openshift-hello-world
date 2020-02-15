b82b014edd64a1d280b8a1ac31e97bd82ed95991
Andreas Landerer <andreas.landerer@gmail.com>
2020-02-15 16:53:32 +0100

    Using other maven image to resolve access problems

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,15 +3,14 @@ String label = "worker-${UUID.randomUUID().toString()}"
 podTemplate(label: label,
             cloud: "openshift",
             containers: [
-                    containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:3.35-5-alpine'),
+                    containerTemplate(
+                            name: 'jnlp',
+                            image: 'jenkins/jnlp-slave:3.35-5-alpine'),
                     containerTemplate(
                             name: 'maven',
                             image: 'maven:3.3.9-jdk-8-alpine',
                             ttyEnabled: true,
                             command: 'cat')
-            ],
-            volumes: [
-                    persistentVolumeClaim(claimName: 'jenkins-maven-cache', mountPath: '/home/jenkins/.m2/repository')
             ]
 ) {
     try {
@@ -28,7 +27,8 @@ podTemplate(label: label,
                     }
 
                     stage('build') {
-                        sh 'mvn clean package'
+                        sh "ls -al"
+                        sh 'mvn clean package -X'
                     }
                 }
             }
