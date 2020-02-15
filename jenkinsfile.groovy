41a700d082d36ec880e2fa4e000d2b6d32e310ba
Andreas Landerer <andreas.landerer@gmail.com>
2020-02-15 15:54:46 +0100

    Executing maven container as root user

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,7 +4,12 @@ podTemplate(label: label,
             cloud: "openshift",
             containers: [
                     containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:3.35-5-alpine'),
-                    containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat')
+                    containerTemplate(
+                            name: 'maven',
+                            image: 'maven:3.3.9-jdk-8-alpine',
+                            ttyEnabled: true,
+                            command: 'cat',
+                            args: ['-u root'])
             ]) {
     try {
         node(label) {
