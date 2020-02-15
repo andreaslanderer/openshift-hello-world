d4da2e5584074c61df90f887236ff6bcb8dded1c
Andreas Landerer <andreas.landerer@gmail.com>
2020-02-15 12:02:12 +0100

    Start using kubernetes jenkins plugin in pipeline

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,10 +1,21 @@
-pipeline {
-    agent { docker { image 'maven:3.3.3' } }
-    stages {
-        stage('build') {
-            steps {
-                sh 'mvn --version'
+String label = "worker-${UUID.randomUUID().toString()}"
+
+podTemplate(label: label,
+            cloud: "openshift") {
+    try {
+        node(label) {
+            timeout(time: 30, unit: 'MINUTES') {
+                stage("environment info") {
+                    sh 'printenv'
+                }
             }
         }
+
+        currentBuild.result = 'SUCCESS'
+
+    } catch (e) {
+        echo "Error occured while executing pipeline: ${e}"
+        currentBuild.result = 'FAILURE'
+        throw e
     }
 }
