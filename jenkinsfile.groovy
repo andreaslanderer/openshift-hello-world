1f617e146b7cf110c7294d020ed77cc712ba1f2a
Andreas Landerer <andreas.landerer@gmail.com>
2020-02-15 12:15:54 +0100

    Adding jnlp container

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,7 +2,10 @@ String label = "worker-${UUID.randomUUID().toString()}"
 
 podTemplate(label: label,
             cloud: "openshift",
-            containers: [containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat')]) {
+            containers: [
+                    containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:3.35-5-alpine'),
+                    containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat')
+            ]) {
     try {
         node(label) {
             timeout(time: 30, unit: 'MINUTES') {
