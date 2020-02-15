2804d5d5faeac7aeb85bad43b933b54cec1f32c1
Andreas Landerer <andreas.landerer@gmail.com>
2020-02-15 12:10:52 +0100

    Added maven container to pipeline

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,7 +1,8 @@
 String label = "worker-${UUID.randomUUID().toString()}"
 
 podTemplate(label: label,
-            cloud: "openshift") {
+            cloud: "openshift",
+            containers: [containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat')]) {
     try {
         node(label) {
             timeout(time: 30, unit: 'MINUTES') {
